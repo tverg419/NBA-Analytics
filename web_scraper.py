e7bfc3dd2593904e4eac9b9470ae795e7e8f7f30
Troi <>
2020-12-09 17:11:57 -0500

    scraping raw data

--- web_scraper.py
+++ web_scraper.py
@@ -1,26 +1,20 @@
 import os
 from urllib.request import urlopen
-
 from bs4 import BeautifulSoup
 import pandas as pd
-# First url is customizable but it extracted data from the first table but the
-# desried table was table 4
-year = 2019
-# url = "https://www.basketball-reference.com/teams/BRK/{}.html".format(year)
-url = "https://www.basketball-reference.com/teams/NJN/stats_basic_totals.html"
+
+year = 2020
+url = "https://www.basketball-reference.com/teams/BRK/{}/gamelog/".format(year)
 html = urlopen(url)
 
 soup = BeautifulSoup(html,features="html.parser")
-
-soup.findAll('tr', limit=2)
-headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
+# Extracts the headers of each table
+soup.findAll('tr', limit=4)
+headers = [th.getText() for th in soup.findAll('tr', limit=2)[1].findAll('th')]
 headers = headers[1:]
-print(headers)
-
+# Extracts Row Data from table and converts into DF
 rows = soup.findAll('tr')[1:]
-team_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))]
-totals = pd.DataFrame(team_stats, columns = headers)
-totals.head(10)
-print(team_stats[20])
+game_log = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))]
+games = pd.DataFrame(game_log, columns = headers)
 
-totals.to_csv('BKN_1967-2019.csv')
+games.to_csv(r'C:\Users\verga\Documents\Projects\nba_analytics\bkn_2019-2020.csv')
